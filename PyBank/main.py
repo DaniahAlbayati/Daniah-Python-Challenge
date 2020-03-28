b61bbcda018af9f118722209d11936b47fd381e9
DaniahAlbayati <60355150+DaniahAlbayati@users.noreply.github.com>
2020-03-28 11:51:54 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,28 +1,44 @@
 import os
 import csv
+total_months = 0
+Net_Total = 0
+Average = 0
+sum = 0
 
-# Creating a path to collect the data from the Resource Folder
-Pybank_csv= os.path.join("..","Resources","budget_data.csv")
+# Creating a path for the file
+csvpath= os.path.join("Resources","budget_data.csv")
 
-# Read the budget_data file as CVS file
-with open (Pybank_csv) as csvfile:
+# check if we found the file
+found=False
+
+# Open the CSV file
+with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-# loop through the date data
-       
+
+# Skip the header
+    csv_header = next(csvfile)
+   
+# loop through the date column  
     for row in csvreader:
-     # add total months
-     Total_Months.append(row[1])
-        
- 
-# Get the Financial Analysis and the line underneath it as per the text example
+        total_months = total_months + 1
+      
+# Get the net total of profit/losses
+        Net_Total = Net_Total+ int(row[1])
+
+ # Get the average change
+
+    
 print ("Financial Analysis")
-print ("....................")
-print ( "Total Months : {row_count}")
+print ("..........................")
+print ("Total Months :" ,(total_months))
+print("Total : $",Net_Total)
+
+
+
+
+  
+
+
 
 
         
-
-
-
-
- 
